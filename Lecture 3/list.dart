49ecaf2b81a2e06ae7801e90c286475db4671243
Emrecan Balgün <deathlygrave@gmail.com>
2022-12-19 16:25:27 +0300

    update list

--- Lecture 3/list.dart
+++ Lecture 3/list.dart
@@ -2,14 +2,14 @@ void main() {
   List<int> moneys = [100, 200, 110];
   final List<int> newMoneys = [10, 20, 30];
   moneys.sort((a, b) => a-b);
-  print(moneys);
+  // print(moneys);
 
   moneys.add(50);
   moneys.insert(2, 250);
-  print(moneys);
+  // print(moneys);
 
   newMoneys.add(5);
-  print(newMoneys);
+  // print(newMoneys);
 
   // newMoneys.clear();
   // print(newMoneys);
@@ -25,13 +25,27 @@ void main() {
   customerMoney2.sort();
   // high to low
 
-  for(int i = 0; i < customerMoney2.length; i++) {
-    if(customerMoney2[i] > 35) {
-      print("Customer ${i} has more than 35");
-    } else if(customerMoney2[i] > 5) {
-      print("Customer ${i} has more than 5");
-    } else {
-      print("Customer ${i} has less than 35");
-    }
-  }
+  // for(int i = 0; i < customerMoney2.length; i++) {
+  //   if(customerMoney2[i] > 35) {
+  //     print("Customer ${i} has more than 35");
+  //   } else if(customerMoney2[i] > 5) {
+  //     print("Customer ${i} has more than 5");
+  //   } else {
+  //     print("Customer ${i} has less than 35");
+  //   }
+  // }
+
+  List<dynamic> user = [1, "Emre", 1.75];
+
+  // for(var item in user) {
+  //   print(item);
+  // }
+
+  List<String> userNames = ["Emre", "Ahmet", "Mehmet"];
+
+  userNames.contains("Emre");
+
+  // userNames.forEach((element) {
+  //   print(element);
+  // }); 
 }
