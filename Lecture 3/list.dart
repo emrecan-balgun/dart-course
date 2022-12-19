07c283d2f74bdc9c161334457d0bb669ac739c71
Emrecan Balgün <deathlygrave@gmail.com>
2022-12-19 15:48:17 +0300

    add list

---
+++ Lecture 3/list.dart
@@ -0,0 +1,37 @@
+void main() {
+  List<int> moneys = [100, 200, 110];
+  final List<int> newMoneys = [10, 20, 30];
+  moneys.sort((a, b) => a-b);
+  print(moneys);
+
+  moneys.add(50);
+  moneys.insert(2, 250);
+  print(moneys);
+
+  newMoneys.add(5);
+  print(newMoneys);
+
+  // newMoneys.clear();
+  // print(newMoneys);
+
+  // newMoneys.reversed.toList();
+
+  List<double> customerMoney = List.generate(100, (index) {
+    return index + 5;
+  });
+  // print(customerMoney);
+
+  List<int> customerMoney2 = [3, 20, 30, 40];
+  customerMoney2.sort();
+  // high to low
+
+  for(int i = 0; i < customerMoney2.length; i++) {
+    if(customerMoney2[i] > 35) {
+      print("Customer ${i} has more than 35");
+    } else if(customerMoney2[i] > 5) {
+      print("Customer ${i} has more than 5");
+    } else {
+      print("Customer ${i} has less than 35");
+    }
+  }
+}
